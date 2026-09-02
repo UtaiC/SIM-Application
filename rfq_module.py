1a5b4e162a53c1cfe2628cb3fff9de5d1a230378
Utai <74049768+UtaiC@users.noreply.github.com>
2026-09-02 10:31:02 +0700

    Add Khun Sombut Mail

--- rfq_module.py
+++ rfq_module.py
@@ -26,7 +26,7 @@ def send_auto_email(rfq_data):
     admin_team = [
         "wattanapon.s@siamintermold.com", "paitoon.b@siamintermold.com",
         "utai.c@siamintermold.com", "rewat.m@siamintermold.com",
-        "admincenter@siamintermold.com", "waiphop.b@siamintermold.com"
+        "admincenter@siamintermold.com", "waiphop.b@siamintermold.com","sombat.k@siamintermold.com"
     ]
     
     staff_team = []
